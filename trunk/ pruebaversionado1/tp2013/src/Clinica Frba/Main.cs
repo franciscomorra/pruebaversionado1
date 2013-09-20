2731bd67d2e67239ee8eecfc60b8b02f16fea248
franciscomorra@gmail.com <franciscomorra@gmail.com@b55918f7-98da-d0db-ded5-c1361162cc21>
2013-09-20 03:37:38 +0000

    Saque la carpeta integrador, no sirve subo la base de la solucion, con los 4 proyectos referenciados
    Tienen que importar la base de datos al sql server y lo del usuario gd para que funcione

---
+++ trunk/ pruebaversionado1/tp2013/src/Clinica Frba/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration; //Para que tome bien click derecho en references y agregar referencia
+
+
+
+using ClinicaFrba.Comun;
+using ClinicaFrba.Negocio;
+using Data;
+
+namespace Clinica_Frba
+{
+    public partial class Main : Form
+    {
+
+
+    }
+}
